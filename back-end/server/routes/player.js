1b449d5f6cb3a4b16be7fdf3fe699e0309517ada
Gareth Fuller <gareth@honestideas.com>
2014-03-28 21:40:41 +0000

    added vector 2d class and mover class, some awesome moving physics from nature of code book

--- back-end/server/routes/player.js
+++
@@ -1,9 +0,0 @@
-/* Socket routes for the player resource */
-
-exports.sockets = function(socket) {
-	
-	socket.on('requestCollition', function (data) {
-		//process the collisoin based on the data
-		socket.emit('collision', { name:'collided' });
-	});
-};
--- front-end/assets/js/app/lib/socket.js
+++
@@ -1,9 +0,0 @@
-/* Websocket test */
-
-// when the server sends a collision event
-core.socket.on('collision', function (data) {
-	console.log(data);
-});
-
-// ask the server got a collision event at x:50
-core.socket.emit('requestCollition', { x: 50 });
